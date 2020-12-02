85c094caade704747ef2aec92ef6bfcb0c363fe6
edsumaneev <edsumaneev@yandex.ru>
2020-12-02 21:22:03 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# _lash_beauty
---
+++ src/assets/js/index.js
@@ -0,0 +1,18 @@
+require('bootstrap');
+require('slick-carousel');
+
+// $('.hamburger').on('click', function (event) {
+//   event.preventDefault();
+//   $(this).toggleClass('active')
+// });
+
+// $('.slider').slick({
+//   slidesToShow: 1,
+//   infinite: true,
+//   autoplay: false,
+//   prevArrow: $(".slick-prev"),
+//   nextArrow: $(".slick-next"),
+//   dots: true,
+//   dotsClass: 'slider__dots'
+// });
+
