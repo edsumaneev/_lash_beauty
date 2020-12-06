bf1113f318eaa9c42cbf48cad6860bfa3ab8b2bb
edsumaneev <edsumaneev@yandex.ru>
2020-12-06 19:39:49 +0300

    06-12

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,3 +1,4 @@
+// 'use strict';
 require('bootstrap');
 require('slick-carousel');
 require('magnific-popup');
@@ -20,12 +21,32 @@ require('magnific-popup');
     }
   });
 
+
+
   // Type Iframe - видео Youtube (или Vimeo), карты Гугл или другой контент в iframe 
-  $('.advantage__youtube').magnificPopup({
-    type: 'iframe'
+  $(document).ready(function () {
+        $('.videobox__youtube, .contacts__map').magnificPopup({
+              disableOn: 700,
+              type: 'iframe',
+              mainClass: 'mfp-fade',
+              removalDelay: 160,
+              preloader: false,
+
+              fixedContentPos: false
+    });
   });
 
-  $('.salon__slider').slick({
+  $(".reviews__slider").slick({
+    slidesToShow: 1,
+    infinite: true,
+    autoplay: false,
+    touchThreshold: 100,
+    dots: true,
+    prevArrow: false,
+    nextArrow: false
+  });
+
+  $(".salon__slider").slick({
     slidesToShow: 2,
     infinite: true,
     autoplay: false,
@@ -33,22 +54,43 @@ require('magnific-popup');
     verticalSwiping: true,
     touchThreshold: 100,
     dots: false,
-    prevArrow: false,
-    nextArrow: false,
-
     prevArrow: $(".slick-prev"),
-    nextArrow: $(".slick-next"),
-    // 
-    // dotsClass: 'slider__dots'
+    nextArrow: $(".slick-next")
   });
 
 })();
-// $('.slider').slick({
-//   slidesToShow: 1,
-//   infinite: true,
-//   autoplay: false,
-//   prevArrow: $(".slick-prev"),
-//   nextArrow: $(".slick-next"),
-//   dots: true,
-//   dotsClass: 'slider__dots'
-// });
+window.addEventListener("DOMContentLoaded", function () {
+  function setCursorPosition(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select()
+    }
+  }
+
+  function template(event) {
+    var matrix = "+39 (___) - ___ - __ - __",
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = this.value.replace(/\D/g, "");
+    if (def.length >= val.length) val = def;
+    this.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+    });
+    if (event.type == "blur") {
+      if (this.value.length == 2) this.value = ""
+    } else setCursorPosition(this.value.length, this)
+  }
+
+  var phoneForm = document.querySelector(".feedback__tel");
+
+  if (phoneForm) {
+    phoneForm.addEventListener("input", template, false);
+    phoneForm.addEventListener("focus", template, false);
+    phoneForm.addEventListener("blur", template, false);
+  }
+});
